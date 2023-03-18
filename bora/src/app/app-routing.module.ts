e0075722dfebe5f38cc6df183c0c4587cbc2b212
System Administrator <root@192.168.1.17>
2023-03-17 21:06:19 -0300

    page of upload picture

--- bora/src/app/app-routing.module.ts
+++ bora/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InitialPageComponent } from './view/initial-page/initial-page.component';
+import { LoginPageComponent } from './view/login-page/login-page.component';
+import { RegisterPageComponent } from './view/register-page/register-page.component';
+import { RegisterPictureComponent } from './view/register-picture/register-picture.component';
+import { ViewComponent } from './view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+  path: '', component: ViewComponent ,
+},
+{
+  path: 'initial', component: InitialPageComponent ,
+},
+{
+  path: 'login', component: LoginPageComponent ,
+},
+{
+  path: 'register', component: RegisterPageComponent ,
+},
+{
+  path: 'picture', component: RegisterPictureComponent ,
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
